bef64eaf142b1fb486c869d89540a0e9d46a142e
visose <vicente@visose.com>
2018-11-02 11:24:50 +0000

    GOScale

--- Assets/Structure.cs
+++ Assets/Structure.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 
 public class Structure : MonoBehaviour
 {
-    public GameObject Cube;
+    public GameObject Box;
+    Vector3 _size;
 
     void Start()
     {
-        Random.InitState(42);
+        var collider = Box.GetComponentInChildren<BoxCollider>();
+        var size = collider.size * collider.transform.localScale.x;
+        _size = size;
 
-        for (int i = 0; i < 10; i++)
+       // _size = new Vector3(0.2f, 0.2f, 0.2f);
+        Random.InitState(46);
+
+        for (int i = 0; i < 20; i++)
         {
-            var go = Instantiate(Cube, this.transform);
+            var go = Instantiate(Box, this.transform);
             //var shiftX = (Random.value - 0.5f) * 0.25f;
             //var shiftZ = (Random.value - 0.5f) * 0.25f;
-            var shift = Random.insideUnitCircle * 0.2f;
-            go.transform.position = new Vector3(shift.x, i + 0.5f, shift.y);
+            var shift = Random.insideUnitCircle * 0.02f;
+            go.transform.position = new Vector3(shift.x, i * _size.y, shift.y);
             go.transform.Rotate(0, Random.value * 180, 0);
         }
     }
