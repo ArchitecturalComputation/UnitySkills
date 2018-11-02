96a090498e93efd5852c9c19cd1d487551cb96d6
visose <vicente@visose.com>
2018-11-02 13:10:26 +0000

    Stop previous coroutine

--- Assets/Structure.cs
+++ Assets/Structure.cs
@@ -6,6 +6,7 @@ public class Structure : MonoBehaviour
 {
     public GameObject Box;
     Vector3 _size;
+    List<GameObject> _boxes = new List<GameObject>();
 
     void Start()
     {
@@ -20,22 +21,35 @@ public class Structure : MonoBehaviour
 
     IEnumerator CreateTower()
     {
+        foreach (var box in _boxes)
+        {
+            Destroy(box);
+        }
+
+        _boxes.Clear();
+
         for (int i = 0; i < 11; i++)
         {
             var go = Instantiate(Box, this.transform);
             var shift = Random.insideUnitCircle * 0.02f;
             go.transform.position = new Vector3(shift.x, i * _size.y, shift.y);
             go.transform.Rotate(0, Random.value * 180, 0);
+            _boxes.Add(go);
             yield return new WaitForSeconds(0.2f);
         }
     }
 
+    IEnumerator _coroutine = null;
 
     void OnGUI()
     {
-       if(GUI.Button(new Rect(20, 20, 100, 40), "Create tower"))
+        if (GUI.Button(new Rect(20, 20, 100, 40), "Create tower"))
         {
-            StartCoroutine(CreateTower());
+            if (_coroutine != null)
+                StopCoroutine(_coroutine);
+
+            _coroutine = CreateTower();
+            StartCoroutine(_coroutine);
         }
     }
 }
