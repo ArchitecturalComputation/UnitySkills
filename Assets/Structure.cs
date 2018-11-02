c7fe3952ee5426b2a948374f6c0342b71f8115d2
visose <vicente@visose.com>
2018-11-02 10:30:27 +0000

    Tower

--- Assets/Structure.cs
+++ Assets/Structure.cs
@@ -8,8 +8,17 @@ public class Structure : MonoBehaviour
 
     void Start()
     {
-        var go = Instantiate(Cube, this.transform);
-        go.transform.position += Vector3.right * 3;
+        Random.InitState(43);
+
+        for (int i = 0; i < 5; i++)
+        {
+            var go = Instantiate(Cube, this.transform);
+            //var shiftX = (Random.value - 0.5f) * 0.25f;
+            //var shiftZ = (Random.value - 0.5f) * 0.25f;
+            var shift = Random.insideUnitCircle * 0.25f;
+            go.transform.position = new Vector3(shift.x, i, shift.y);
+            go.transform.Rotate(0, Random.value * 180, 0);
+        }
     }
 
     void Update()
