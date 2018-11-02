8386eeec5eb6bf60f518a8f3e79a09345e5a2c91
visose <vicente@visose.com>
2018-11-02 11:44:27 +0000

    Coroutines

--- Assets/Structure.cs
+++ Assets/Structure.cs
@@ -13,17 +13,20 @@ public class Structure : MonoBehaviour
         var size = collider.size * collider.transform.localScale.x;
         _size = size;
 
-       // _size = new Vector3(0.2f, 0.2f, 0.2f);
         Random.InitState(46);
 
-        for (int i = 0; i < 20; i++)
+        StartCoroutine(CreateTower());
+    }
+
+    IEnumerator CreateTower()
+    {
+        for (int i = 0; i < 11; i++)
         {
             var go = Instantiate(Box, this.transform);
-            //var shiftX = (Random.value - 0.5f) * 0.25f;
-            //var shiftZ = (Random.value - 0.5f) * 0.25f;
             var shift = Random.insideUnitCircle * 0.02f;
             go.transform.position = new Vector3(shift.x, i * _size.y, shift.y);
             go.transform.Rotate(0, Random.value * 180, 0);
+            yield return new WaitForSeconds(0.5f);
         }
     }
 
