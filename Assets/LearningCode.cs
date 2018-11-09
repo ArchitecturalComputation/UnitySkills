a0d8e2a72891f9e2795557fefd5c11d78ecb4fb1
visose <vicente@visose.com>
2018-11-09 10:49:14 +0000

    classes

--- Assets/LearningCode.cs
+++ Assets/LearningCode.cs
@@ -5,32 +5,36 @@ using UnityEngine;
 public class LearningCode : MonoBehaviour
 {
     public GUISkin uiSkin;
-    public string text;
+    public string text = "";
 
     void Start()
     {
-        var numbers = new List<int>();
+        // "value types": int,float, Rect, Vector3, Vector2,
+        // "reference types":  int[], List<int>, uiSkin, gameobject
 
-        for (int i = 0; i < 10; i++)
+        int result = 0;
+
+
+
+        var animals = new List<Animal>();
+        animals.Add(new Cat());
+        animals.Add(new Dog());
+       // animals.Add(new Vehicle());
+
+        var dog = new Dog();
+        dog.Fetch();
+
+        foreach(var animal in animals)
         {
-            if (i == 5)
-                continue;
-
-            numbers.Add(i);
+            //animal.Move(new Vector3(2, 5, 3));
+            //animal.ChopLeg();
+            text += $"Animal has {animal.Legs} legs left. ";
         }
 
-        numbers[0] = 5;
+        // myAnimal.Sound = null;
+        // var sound = myAnimal.Sound;
 
-        var numbersB = numbers;
-        numbers[0] = 10;
-
-       // "value types": int,float, Rect, Vector3, Vector2,
-       // "reference types":  int[], List<int>, uiSkin, gameobject
-
-        ModifyList(numbers);
-        int result = numbers[0];
-      
-        text = $"{result}";
+       
 
         // text = string.Join(", ", numbers);
     }
@@ -45,6 +49,105 @@ public class LearningCode : MonoBehaviour
     void ModifyList(List<int> n)
     {
         n = new List<int>();
-      //  n[0] = 6;
+        //  n[0] = 6;
     }
 }
+
+class Animal : IMovable
+{
+    protected int _legs;
+    int _eyes;
+    Vector3 _position;
+
+    private string _sound;
+
+    public int Legs => _legs;
+
+    public string Sound
+    {
+        get
+        {
+            return _sound;
+        }
+
+        set
+        {
+            if (value == null)
+                _sound = "invalid sound";
+            else
+                _sound = value;
+        }
+    }
+
+    public Animal(int legs, int eyes, string sound)
+    {
+        _legs = legs;
+        _eyes = eyes;
+        _sound = sound;
+
+    }
+
+    public virtual void ChopLeg()
+    {
+        if (_legs == 0) return;
+        _legs -= 1;
+    }
+
+    public void Move(Vector3 delta)
+    {
+        _position += delta;
+    }
+
+    //public string MakeSound()
+    //{
+    //    return _sound;
+    //}
+}
+
+class Dog : Animal
+{
+    public Dog() : base(4, 2, "Woof")
+    {
+
+    }
+
+    public override void ChopLeg()
+    {
+        if (_legs >= 2)
+        _legs -= 2;
+    }
+
+    public void Fetch()
+    {
+
+    }
+}
+
+class Cat : Animal
+{
+    public Cat() : base(4, 2, "Miao")
+    {
+
+    }
+}
+
+
+class Vehicle : IMovable
+{
+    int _wheels;
+    Vector3 _position;
+
+    public void Move(Vector3 delta)
+    {
+        _position += delta;
+    }
+}
+
+enum WeekDay { Mon, Tue, Wed, Thu, Fri }
+
+interface IMovable
+{
+    void Move(Vector3 delta);
+}
+
+
